a53409d61ffc317fc5009ca25ae3f4bf9685ddbf
David Solomon <dav.soli22@gmail.com>
2020-12-16 14:46:28 +0100

    Prepared partner data

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from partner_data_reader import PartnerDataReader
+
+if __name__ == "__main__":
+    partner_id = 0
+    partner_data_reader = PartnerDataReader(partner_id)
---
+++ src/partner_data_reader.py
@@ -0,0 +1,11 @@
+
+class PartnerDataReader:
+    def __init__(self, partner_id):
+        print('PartnerDataReader')
+        print(partner_id)
+        # koszt jednego klikniecia
+        # timestamp to date
+        # dla danego agregacja klikniec
+
+    def next_day():
+        print('next_day')
